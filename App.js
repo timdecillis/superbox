3ff692bd1d6f6edf00d27d4be9f4bf919e348c6b
Joseph Camarena <pepecamarena5@gmail.com>
2023-07-29 17:03:53 -0700

    Created general signin / sign up page and added a logo

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Signin from './pages/Signin';
+import styled from 'styled-components/native'
+
+const Container = styled.View`
+display:flex;
+flex-direction: column;
+height:100%;
+  background-color:#E4B363;
+  align-items: center;
+  justify-content: center;
+`;
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Container>
+      <Signin />
+    </Container>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- pages/Signin.js
+++ pages/Signin.js
@@ -1,9 +1,86 @@
-import React from "react";
+import React, {useState} from "react";
+import styled from 'styled-components/native'
+import Logo from '../assets/logo.png'
+
+
+const Container = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+`;
+
+const LogoImage = styled.Image`
+  width: 75px;
+  height: 75px;
+  margin-bottom: 20px;
+`;
+
+const Title = styled.Text`
+  font-size: 24px;
+  font-weight: bold;
+  margin-bottom: 10px;
+  color: #EF6461;
+`;
+
+const BoldText = styled.Text`
+  font-weight: bold;
+`;
+
+
+const SubHeader = styled.Text`
+  font-size: 18px;
+  margin-bottom: 20px;
+`;
+
+const ButtonContainer = styled.View`
+  flex-direction: row;
+`;
+
+const Button = styled.TouchableOpacity`
+  background-color: #2196F3;
+  padding: 10px 20px;
+  border-radius: 5px;
+  margin: 10px;
+`;
+
+const ButtonText = styled.Text`
+  color: #ffffff;
+  font-size: 16px;
+`;
+
 
 const Signin = () => {
+  const [logIn, setLogin] = useState(false);
+  const [signUp, setSignUp] = useState(false)
+
+  const handleClick = (input) => {
+    if (input === 'logIn') {
+      setLogin(true);
+      setSignUp(false);
+
+    } else {
+      setLogin(false);
+      setSignUp(true);
+    }
+
+  };
 
   return (
+    <Container>
+      <LogoImage source={Logo} />
+      <Title>Welcome to <BoldText>SUPERBOX</BoldText></Title>
+      <SubHeader>Login to continue</SubHeader>
+      <ButtonContainer>
+              <Button onPress={() => {handleClick('logIn')}}>
+        <ButtonText>Log In</ButtonText>
+      </Button>
+      <Button onPress={() => {handleClick('signUp')}}>
+        <ButtonText>Sign Up</ButtonText>
+      </Button>
+      </ButtonContainer>
 
+  </Container>
   )
 }
 export default Signin;
