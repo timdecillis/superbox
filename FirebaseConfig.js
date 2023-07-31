e18cc250bad071dcbf21078494fefd56d63392a2
Joseph Camarena <pepecamarena5@gmail.com>
2023-07-31 11:44:39 -0700

    config.js to include firebaseconfig

--- FirebaseConfig.js
+++ FirebaseConfig.js
@@ -2,19 +2,11 @@
 import { initializeApp } from "firebase/app";
 import React, { useState, useEffect } from 'react'
 import { getAuth, onAuthStateChanged, User } from 'firebase/auth'
+import firebaseConfig from '../Superbox/config'
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyAyJaKkBQAGEIeC08F_dKCudivRhEXWUiA",
-  authDomain: "blue-ocean-2c189.firebaseapp.com",
-  projectId: "blue-ocean-2c189",
-  storageBucket: "blue-ocean-2c189.appspot.com",
-  messagingSenderId: "485196925549",
-  appId: "1:485196925549:web:70467ba9c48b2f9a5a0d64"
-};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAyJaKkBQAGEIeC08F_dKCudivRhEXWUiA",
+  authDomain: "blue-ocean-2c189.firebaseapp.com",
+  projectId: "blue-ocean-2c189",
+  storageBucket: "blue-ocean-2c189.appspot.com",
+  messagingSenderId: "485196925549",
+  appId: "1:485196925549:web:70467ba9c48b2f9a5a0d64"
+};
