5e70168374530d87606eeccff13057fe4e356503
Oscar Carvente <oscar.carvente@gmail.com>
2023-07-31 15:41:34 -0700

    Add images to cart card

--- features/Cart/CartCard.js
+++ features/Cart/CartCard.js
@@ -5,7 +5,8 @@ const CartCard = ({product}) => {
   return (
     <View>
       <Image
-      source={{uri: product.product_image}} />
+      source={{uri: product.product_image}}
+      style={{width: 50, height: 50}} />
       <Text>{product.product_name}</Text>
       <Text>Description: {product.product_description}</Text>
       <Text>Price: ${product.product_price}</Text>
