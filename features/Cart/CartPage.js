adddbcf0757d76503245fd54cb8d07000b4bd260
Oscar Carvente <oscar.carvente@gmail.com>
2023-07-31 15:44:17 -0700

    Scroll through items in cart

--- features/Cart/CartPage.js
+++ features/Cart/CartPage.js
@@ -8,7 +8,11 @@ const CartPage = () => {
   const[products, setProducts] = useState([
     {product_id: 1, product_name: "Spiderman Version 2.0", product_price: "200", product_image:'https://cdn.marvel.com/u/prod/marvel/i/mg/3/c0/63863d142d94e/clean.jpg', product_description: "Great condition spiderman comic", product_quantity: "4"},
     {product_id: 2, product_name: "Spiderman Version 2.0", product_price: "200", product_image:'https://cdn.marvel.com/u/prod/marvel/i/mg/3/c0/63863d142d94e/clean.jpg', product_description: "Great condition spiderman comic", product_quantity: "4"},
-    {product_id: 3, product_name: "Spiderman Version 2.0", product_price: "200", product_image:'https://cdn.marvel.com/u/prod/marvel/i/mg/3/c0/63863d142d94e/clean.jpg', product_description: "Great condition spiderman comic", product_quantity: "4"}]);
+    {product_id: 3, product_name: "Spiderman Version 2.0", product_price: "200", product_image:'https://cdn.marvel.com/u/prod/marvel/i/mg/3/c0/63863d142d94e/clean.jpg', product_description: "Great condition spiderman comic", product_quantity: "4"},
+    {product_id: 4, product_name: "Spiderman Version 2.0", product_price: "200", product_image:'https://cdn.marvel.com/u/prod/marvel/i/mg/3/c0/63863d142d94e/clean.jpg', product_description: "Great condition spiderman comic", product_quantity: "4"},
+    {product_id: 5, product_name: "Spiderman Version 2.0", product_price: "200", product_image:'https://cdn.marvel.com/u/prod/marvel/i/mg/3/c0/63863d142d94e/clean.jpg', product_description: "Great condition spiderman comic", product_quantity: "4"}
+
+  ]);
 
   const calculateTotal = () => {
     return products.reduce((accu,product) => accu + Number(product.product_price),  0)
