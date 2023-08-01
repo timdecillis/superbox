c3fbf427b813445d6067fc6474eadc0bb4ea5078
Joseph Camarena <pepecamarena5@gmail.com>
2023-07-31 18:22:07 -0700

    lower case l

--- features/LoginSignup/Signin.js
+++ features/LoginSignup/Signin.js
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import { TouchableOpacity } from 'react-native';
 import styled from 'styled-components/native'
-import Logo from '../../assets/Logo.png'
+import Logo from '../../assets/logo.png'
 import { Firebase_Auth} from '../../FirebaseConfig.js'
 import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from 'firebase/auth'
 import {Container, NoAccountContainer, LogoImage, Title, BoldText, SubHeader, ButtonContainer, Button, ContinueBtn, ButtonText, InputBarContainer, InputField, SignUpText, NoAccountText} from './SignIn_Styles'
