eecb45706e76cf5bc1c9ed626ce879d2381058ae
Tim DeCillis <timdecillis@gmail.com>
2023-08-01 12:20:14 -0700

    Added route for public profile

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import Signin from "./features/LoginSignup/Signin.js";
 import UserProfile from './features/UserProfile/UserProfile.js';
 import SignIn from "./features/LoginSignup/Signin.js";
 import MyListings from './features/MyListings/MyListings.js';
+import PublicProfile from './features/UserProfile/PublicProfile.js';
 
 const Tab = createBottomTabNavigator();
 
@@ -18,6 +19,7 @@ export default function App() {
         <Tab.Screen name="Settings" component={Signin} />
         <Tab.Screen name="UserProfile" component={UserProfile} />
         <Tab.Screen name="My Listings" component={MyListings} />
+        <Tab.Screen name="Public Profile" component={PublicProfile} />
       </Tab.Navigator>
     </NavigationContainer>
 
