7031367240f896e7a0138c76d932a7dc195984cd
Joseph Camarena <pepecamarena5@gmail.com>
2023-08-02 12:10:58 -0700

    update api url

--- features/LoginSignup/Signin.js
+++ features/LoginSignup/Signin.js
@@ -18,7 +18,7 @@ const SignIn = ({profile, setProfile}) => {
 
   const sendProfileData = async () => {
     try {
-      const endpoint = '/https://3.141.17.132/u/users/';
+      const endpoint = '/http://3.141.17.132/u/users/';
 
       const config = {
         headers: {
@@ -57,7 +57,7 @@ const SignIn = ({profile, setProfile}) => {
       },
     };
 
-    const backendResponse = await axios.get('/https://3.141.17.132/u/users/:user_id', config);
+    const backendResponse = await axios.get('/http://3.141.17.132/u/users/:user_id', config);
 
     setProfile({
       ...profile,
