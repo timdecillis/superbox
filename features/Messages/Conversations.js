cb81d534595b06733bf2f922f930ceacfbe1b185
Patrick Nilan <nilan.patrick@gmail.com>
2023-08-03 16:13:26 -0700

    Created barebones messages

--- features/Messages/Conversations.js
+++ features/Messages/Conversations.js
@@ -2,47 +2,119 @@ import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, ScrollView, FlatList } from 'react-native';
 import Messages from './Messages.js';
 import { GlobalView } from '../../globalComponents/globalStyles.js';
+import moment from 'moment';
+import axios from 'axios';
 
-const Conversations = ({ currentUser }) => {
+// const customLanguage = {
+//   s: 's', // seconds
+//   m: 'min', // minutes
+//   h: 'hr', // hours
+//   d: 'd', // days
+//   w: 'w', // weeks
+//   M: 'mo', // months
+//   y: 'y' // years
+// };
 
-  const [conversationsArray, setConversationsArray] = useState([]);
-  const [activeConversation, setActiveConversations] = useState([]);
+// moment.updateLocale('en', { relativeTime: customLanguage });
+
+const Conversations = ({ currentUser, profile, setProfile, navigation, handleProfileUpdate }) => {
+
+  let [conversationsArray, setConversationsArray] = useState([]);
+  const [activeConversation, setActiveConversation] = useState({});
+  const [conversationSelected, setConversationSelected] = useState(false);
   const [inputValue, setInputValue] = useState('');
 
-  return (
-    <SafeAreaView style={bodyStyles.safeView} >
-        <View style={headerStyles.headerContainer}>
-          <View style={headerStyles.textContainer}>
-            <Text style={headerStyles.headerText}>
-              Back
+  conversationsArray = [{ username: 'Patrick', content: 'Hello how are you? I would like to buy a comic.', created_at: '2023-08-02T21:38:29Z'}, { username: 'Patrick', content: 'Hello how are you? I would like to buy a comic.', created_at: '2023-08-02T20:38:29Z'}, { username: 'Patrick', content: 'Hello how are you? I would like to buy a comic.', created_at: '2023-08-02T19:38:29Z'}, { username: 'Patrick', content: 'Hello how are you? I would like to buy a comic.', created_at: '2023-08-02T18:38:29Z'}];
+
+  const renderConversation = ({item}) => {
+
+    const dateTimeAgo = moment(item.created_at).fromNow();
+    // const dateTimeAgo = moment('2023-08-03T20:40:54+0000').fromNow();
+
+    return (
+      <TouchableOpacity onPress={() => {
+        setActiveConversation(item);
+        setConversationSelected(true);
+      }}>
+        <View style={listStyles.container}>
+          <View style={listStyles.icon}>
+            <Text style={listStyles.iconText}>
+              P
             </Text>
           </View>
-          <View style={headerStyles.titleContainer}>
-            <Text style={headerStyles.titleText}>
-              Messages
-            </Text>
-          </View>
-          <View style={headerStyles.textContainer}>
-            <Text style={headerStyles.headerText}>
-              Cart
-            </Text>
+          <View style={listStyles.textContainer}>
+            <View style={listStyles.messageContainer}>
+              <Text numberOfLines={1} style={listStyles.messageText}>
+                Hello how are you? I would like to buy a comic.
+              </Text>
+            </View>
+            <View style={listStyles.timeContainer}>
+              <Text style={listStyles.timeText}>
+                {dateTimeAgo}
+              </Text>
+            </View>
           </View>
         </View>
-    </SafeAreaView>
-  );
+      </TouchableOpacity>
+    );
+  };
+
+  if (!conversationSelected) {
+    return (
+      <SafeAreaView style={bodyStyles.safeView} >
+          <View style={headerStyles.headerContainer}>
+            <TouchableOpacity style={headerStyles.textContainer} onPress={() => navigation.navigate('User Profile', {
+                  currentUser: profile,
+                  navigation: navigation,
+                  profile: profile,
+                  setProfile: setProfile,
+                  handleProfileUpdate: handleProfileUpdate
+                })}>
+              <Text style={headerStyles.headerText}>
+                Back
+              </Text>
+            </TouchableOpacity>
+            <View style={headerStyles.titleContainer}>
+              <Text style={headerStyles.titleText}>
+                Messages
+              </Text>
+            </View>
+            <TouchableOpacity style={headerStyles.textContainer} onPress={() => navigation.navigate('Cart')}>
+              <Text style={headerStyles.headerText}>
+                Cart
+              </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={bodyStyles.bodyContainer}>
+            <FlatList
+                data={conversationsArray}
+                renderItem={renderConversation}
+                keyExtractor={(item, index) => index}
+                showsVerticalScrollIndicator={false}
+              />
+          </View>
+      </SafeAreaView>
+    );
+  } else {
+    return (
+      <Messages currentUser={currentUser} activeConversation={activeConversation} setActiveConversation={setActiveConversation} setConversationSelected={setConversationSelected} navigation={navigation} />
+    );
+  }
 };
+
+
+
 
 const headerStyles = StyleSheet.create({
   headerContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
-    borderColor: 'grey',
+    borderColor: '#ccc',
     borderBottomWidth: 1
   },
   textContainer: {
     height: 50,
-    width: 50,
-    backgroundColor: 'white',
+    width: 70,
     justifyContent: 'center',
     alignItems: 'center'
   },
@@ -52,10 +124,12 @@ const headerStyles = StyleSheet.create({
   titleContainer: {
     height: 50,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    flexWrap: 'wrap'
   },
   titleText: {
-    fontSize: 24
+    fontSize: 24,
+    flexWrap: 'wrap'
   }
 });
 
@@ -63,8 +137,62 @@ const bodyStyles = StyleSheet.create({
   safeView: {
     flex: 1,
     backgroundColor: '#FDFAF4'
+  },
+  bodyContainer: {
+    flex: 1
   }
 });
+
+const listStyles = StyleSheet.create({
+  container: {
+    marginTop: 10,
+    marginLeft: 10,
+    marginRight: 10,
+    flexDirection: 'row',
+    height: 70,
+
+  },
+  icon: {
+    height: 70,
+    width: 70,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#E4B363'
+  },
+  iconText: {
+    fontSize: 48
+  },
+  textContainer: {
+    width: 300,
+    height: 70,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingLeft: 10,
+    marginRight: 10
+  },
+  messageContainer: {
+    height: '100%',
+    width: 240,
+    justifyContent: 'center'
+  },
+  messageText: {
+    fontSize: 16
+  },
+  timeContainer: {
+    height: 50,
+    width: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  timeText: {
+    fontSize: 10,
+    color: 'grey',
+    textAlign: 'center',
+  }
+});
+
+
 
 export default Conversations;
 
