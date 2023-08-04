25fefb50a180d1855ba07e81c8a20cd2d4dc0ebe
Oscar Carvente <oscar.carvente@gmail.com>
2023-08-04 08:46:51 -0700

    Make request to cart api for cart information

--- features/Cart/CartCard.js
+++ features/Cart/CartCard.js
@@ -3,17 +3,15 @@ import {CardContainer} from './styles.js';
 import {GlobalPrice} from '../../globalComponents/globalStyles.js'
 const CartCard = ({product}) => {
 
+  const photoURL = (product.photos &&  product.photos[0]) ? product.photos[0].photo_url : 'https://cdn.marvel.com/u/prod/marvel/i/mg/3/c0/63863d142d94e/clean.jpg' ;
+
   return (
     <CardContainer >
       <Image
-        source={{uri: product.product_image}}
+        source={{uri:photoURL }}
         style={{width: 50, height: 45, marginRight: 20}} />
-      <Text style={{ marginRight: 20}}>{product.product_name}</Text>
-      <GlobalPrice> ${product.product_price}</GlobalPrice>
-
-
-
-
+      <Text style={{ marginRight: 20}}>{product.description}</Text>
+      <GlobalPrice> {product.price} </GlobalPrice>
     </CardContainer>
   )
 }
