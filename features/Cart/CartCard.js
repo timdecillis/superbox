72d8b80d5800bc1331030080d2b10e6c80440f6c
Tim DeCillis <timdecillis@gmail.com>
2024-01-11 15:28:49 -0800

    added image to cart page

--- features/Cart/CartCard.js
+++ features/Cart/CartCard.js
@@ -5,10 +5,12 @@ import {GlobalPrice} from '../../globalComponents/globalStyles.js'
 
 const CartCard = ({product}) => {
 
+  console.log('image url:', product.photo_url)
+
   return (
     <CardContainer >
       <Image
-        source={{uri:product.photo_url }}
+        source={{uri:product.product_image }}
         style={{width: 50, height: 45, marginRight: 20}} />
       <Text style={{ marginRight: 20}}>{product.product_name}</Text>
       <GlobalPrice> {product.price} </GlobalPrice>
